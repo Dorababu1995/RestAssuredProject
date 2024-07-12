9a266d5831b75a6da34af408883c47ea80e3f8d8
Chodavarapu Dorababu <chodavarapu.dorababu@nagarro.com>
2024-07-12 13:59:34 +0530

    YOUR FIRST MESSAGE HERE

---
+++ src/test/java/BDD/Get_Demo1.java
@@ -0,0 +1,29 @@
+package BDD;
+
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matcher.*;
+
+import org.junit.BeforeClass;
+import org.testng.annotations.Test;
+
+
+
+public class Get_Demo1 {
+
+	@BeforeClass
+	private void tcclass() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	@Test
+	private void tc1() {
+		
+		System.out.println("test case passed");
+		
+	}
+
+
+}
---
+++ src/test/java/BDD/sample.java
@@ -0,0 +1,14 @@
+package BDD;
+
+import org.testng.annotations.Test;
+
+public class sample {
+
+	
+	
+	@Test
+	private void tc() {
+		
+
+	}
+}
