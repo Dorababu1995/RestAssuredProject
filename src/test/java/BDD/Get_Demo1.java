af67e8f8e29539b05e7e89e986fab325bcdbb1ae
Chodavarapu Dorababu <chodavarapu.dorababu@nagarro.com>
2024-07-12 16:45:29 +0530

    selenium

--- src/test/java/BDD/Get_Demo1.java
+++ src/test/java/BDD/Get_Demo1.java
@@ -5,25 +5,62 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matcher.*;
 
 import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 
 public class Get_Demo1 {
 
-	@BeforeClass
-	private void tcclass() {
-		// TODO Auto-generated method stub
+	public static WebDriver driver;
+	
+	
+	@BeforeSuite
+	private void SuiteExection() {
+		
+		System.out.println("SUITE STARTED");
+
+	}
+	
+	@BeforeTest
+	private void DriverPreparaion() {
+		
+		
+		System.setProperty("webdriver.chrome.driver","C:/Users/chodavarapudorababu/Downloads/drivers/drivers/chromedriver-win64/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.facebook.com/");
+		
 
 	}
 	
 	
-	@Test
-	private void tc1() {
+	
+	
+	@Test(description = "validate the title of the page")
+	private void TC_1() {
 		
-		System.out.println("test case passed");
+		String title = driver.getTitle();
 		
+		System.out.println(title);
+
 	}
-
-
+	
+	@AfterTest
+	private void Tear() {
+	
+		
+		driver.quit();
+	}
+	
+	
+	
 }
